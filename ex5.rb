2ddba9a1900260ed9e5a6489781cae4ad55069cc
alicetragedy <girl@alicetragedy.org>
2015-05-25 15:39:37 +0200

    study drill: add variables for unit conversion (cm and kg)

--- ex5.rb
+++ ex5.rb
@@ -5,10 +5,12 @@ weight = 180 # lbs
 eyes = 'blue'
 teeth = 'white'
 hair = 'brown'
+cm = height / 0.39370
+kg = weight / 2.2046
 
 puts "Let's talk about #{name}."
-puts "He's #{height} inches tall."
-puts "He's #{weight} pounds heavy."
+puts "He's #{height} inches, or #{cm} centimeters, tall."
+puts "He's #{weight} pounds, or #{kg} kilograms, heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{eyes} eyes and #{hair} hair."
 puts "His teeth are usually #{teeth} depending on the coffee."
